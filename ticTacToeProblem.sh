e18dc01cfa780ce8d49c38c51d21910e8aa81379
Admin <none@gmail.com>
2020-03-11 09:22:05 +0530

    Added: Showing Which Letter Assigned To Players

--- ticTacToeProblem.sh
+++ ticTacToeProblem.sh
@@ -5,41 +5,43 @@
 # @ Version: 18.04.3 lts
 # @ Since: 11 March 2020 / Wednesday
 
-# Use Case 1
-
 declare -A board
-
-# Variables
 
 noOfRows=3
 noOfColumns=3
 
-initializeBoard()
-{
-	for (( r=1; r<=$noOfRows; r++ ))
-	do
-		for ((c=1; c<=$noOfColumns; c++ ))
-		do
+initializeBoard(){
+	for (( r=1; r<=$noOfRows; r++ )); do
+		for ((c=1; c<=$noOfColumns; c++ ));	do
 			board[r,c]="+"
 		done
 	done
 }
 
-displayBoard()
-{
-	initializeBoard
-
-	for (( r=1; r<=$noOfRows; r++ ))
-	do
+displayBoard(){
+	for (( r=1; r<=$noOfRows; r++ )); do
 		echo "---------------"
-		for (( c=1; c<=$noOfColumns; c++ ))
-		do
+		for (( c=1; c<=$noOfColumns; c++ )); do
 			printf "| ${board[r,c]} |"
 		done
 		echo
 	done
 	echo "---------------"
-
 }
 
+letterAssign(){
+	if [[ $((RANDOM%2)) -eq 0 ]];	then
+		playerOne=X
+		PlayerTwo=O
+		echo "Player1 ==> X"
+		echo "Player2 ==> O"
+	else
+		playerOne=O
+      PlayerTwo=X
+      echo "Player1 ==> O"
+      echo "Player2 ==> X"
+	fi
+}
+initializeBoard
 displayBoard
+letterAssign
