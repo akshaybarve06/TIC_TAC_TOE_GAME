f4c4c6e5ca250394b2559ddcd8747af72e625fa2
Admin <none@gmail.com>
2020-03-11 15:20:22 +0530

    Added: Playing Game With Computer

--- ticTacToeProblem.sh
+++ ticTacToeProblem.sh
@@ -13,17 +13,17 @@ noOfRows=3
 noOfColumns=3
 
 initializeBoard(){
-	for (( r=1; r<=$noOfRows; r++ )); do
-		for ((c=1; c<=$noOfColumns; c++ ));	do
-			board[$r,$c]="+"
+	for (( r=0; r<$noOfRows; r++ )); do
+		for ((c=0; c<$noOfColumns; c++ ));	do
+			board[$r,$c]="-"
 		done
 	done
 }
 
 displayBoard(){
-	for (( r=1; r<=$noOfRows; r++ )); do
+	for (( r=0; r<$noOfRows; r++ )); do
 		echo "---------------"
-		for (( c=1; c<=$noOfColumns; c++ )); do
+		for (( c=0; c<$noOfColumns; c++ )); do
 			printf "[ ${board[$r,$c]} ]"
 		done
 		echo
@@ -36,98 +36,101 @@ letterAssign(){
       playerTwo="O"
 }
 
-toss(){
-	if [[ $((RANDOM%2)) -eq 0 ]]; then
-		echo "Player1 Will Play First"
-	else
-		echo "Player2 Will Play First"
-	fi
-}
-
 startToPlay(){
 	letterAssign
-	while [[ $turn -lt 9 && isWon -eq 0 ]]
+	while [[ $turn -lt 9 && $isWon -eq 0 ]]
 	do
-		if [[ $(($turn%2)) -eq 0 ]]; then
-			read -p "Player 1 Enter Position" position
-			player=$playerOne
-		else
-			read -p "Player 2 Enter Position" position
-			player=$playerTwo
-		fi
-		case $position in
-			1)
-				board[1,1]=$player;;
-			2)
-            board[1,2]=$player;;
+	if [[ $(($turn%2)) -eq 0 ]]; then
+		read -p "Player 1 Enter Position" position
+		player=$playerOne
+	else
+		position=$(computerTurn)
+		player=$playerTwo
+	fi
+	case $position in
+	0)
+		board[0,0]=$player;;
+	1)
+            board[0,1]=$player;;
+         2)
+            board[0,2]=$player;;
          3)
-            board[1,3]=$player;;
+            board[1,0]=$player;;
          4)
-            board[2,1]=$player;;
+            board[1,1]=$player;;
          5)
-            board[2,2]=$player;;
+            board[1,2]=$player;;
          6)
-            board[2,3]=$player;;
+            board[2,0]=$player;;
          7)
-            board[3,1]=$player;;
+            board[2,1]=$player;;
          8)
-            board[3,2]=$player;;
-         9)
-            board[3,3]=$player;;
-			*)
-				echo Invalid Position
-				((turn--))
-			;;
-		esac
-		((turn++))
-		checkWin
-		displayBoard
-		if [[ $player -eq X && $isWon -eq 1 ]]; then
-         echo "Player 1 Won.."
-      elif [[ $player -eq O && $isWon -eq 1 ]]; then
-         echo "Player 2 Won.."
-      fi
-		if [[ $turn -eq 9 ]]; then
-         echo "Match Tied"
-	   fi
+            board[2,2]=$player;;
+	*)
+		echo Invalid Position
+		((turn--))
+	;;
+	esac
+	((turn++))
+	checkWin
+	displayBoard
+	if [[ $player == X && $isWon -eq 1 ]]; then
+ 		echo "Player 1 Won.."
+	elif [[ $player == O && $isWon -eq 1 ]]; then
+ 		echo "Player 2 (Computer) Won.."
+	elif [[ $turn -eq 9 ]]; then
+ 		echo "Match Tied"
+   	fi
 	done
 }
 
 checkWin(){
-	if [[ ${board[1,1]} == $player && ${board[1,2]} == $player &&  ${board[1,3]} == $player ]]; then
+	if [[ ${board[0,0]} == $player && ${board[0,1]} == $player &&  ${board[0,2]} == $player ]]; then
 		isWon=1
-	elif [[ ${board[2,1]} == $player && ${board[2,2]} == $player &&  ${board[2,3]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[3,1]} == $player && ${board[3,2]} == $player &&  ${board[3,3]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[1,1]} == $player && ${board[2,1]} == $player &&  ${board[3,1]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[1,2]} == $player && ${board[2,2]} == $player &&  ${board[3,2]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[1,3]} == $player && ${board[2,3]} == $player &&  ${board[3,3]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[1,1]} == $player && ${board[2,2]} == $player &&  ${board[3,3]} == $player ]]; then
-      isWon=1
-	elif [[ ${board[1,3]} == $player && ${board[2,2]} == $player &&  ${board[3,3]} == $player ]]; then
-      isWon=1
+	elif [[ ${board[1,0]} == $player && ${board[1,1]} == $player &&  ${board[1,2]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[2,0]} == $player && ${board[2,1]} == $player &&  ${board[2,2]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[0,0]} == $player && ${board[1,0]} == $player &&  ${board[2,0]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[0,1]} == $player && ${board[1,1]} == $player &&  ${board[2,1]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[0,2]} == $player && ${board[1,2]} == $player &&  ${board[2,2]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[0,0]} == $player && ${board[1,1]} == $player &&  ${board[2,2]} == $player ]]; then
+      		isWon=1
+	elif [[ ${board[0,2]} == $player && ${board[1,1]} == $player &&  ${board[2,0]} == $player ]]; then
+      		isWon=1
 	fi
 	if [[ $turns -eq 9 ]]; then
-         echo "Match Tied"
+	 echo "Match Tied"
 	fi
 }
 
+computerTurn(){
+		generatedNum=$((RANDOM%8))
+		r=$(($generatedNum/3))
+      		c=$(($generatedNum%3))
+		if [[ ${board[$r,$c]} ==  X || ${board[$r,$c]} == O ]]; then
+			computerTurn
+		elif [[ ${board[$r,$c]} == - ]]; then
+			cPosition=$generatedNum
+		fi
+	echo $cPosition
+}
+
 wantToPlay(){
-   read -p "Would You Like To Toss Y/N ?" isToss
-   if [[ $isToss == Y || $isToss == y ]]; then
-#      toss
-		echo "Player 1 Will Play First"
-      startToPlay
+   read -p "Would You Like To Start game Y/N ?" isStart
+   if [[ $isStart == Y || $isStart == y ]]; then
+	echo "Okay..Starting Game"
+      	startToPlay
    else
-      echo "Okay..Starting Game"
-      startToPlay
+      	echo "Exiting.."
+			exit
    fi
 }
 
 initializeBoard
 displayBoard
 wantToPlay
+
